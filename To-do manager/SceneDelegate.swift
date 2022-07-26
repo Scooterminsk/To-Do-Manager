c962e4bc4f4a2150caf5aad680d5fb40facf9ef3
Scooterminsk <kickscooterby@gmail.com>
2022-07-26 18:18:34 +0300

    The most important functions of this app were added

--- To-do manager/SceneDelegate.swift
+++ To-do manager/SceneDelegate.swift
@@ -13,10 +13,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        guard let window = window else {
+            return
+        }
+
+        // loading the task list
+        // it has to be done before creation a class object
+        // in another case data will be reloaded
+        let tasks = TaskStorage().loadTasks()
+        
+        // loading of the scene with the task list
+        let taskListController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TaskListController") as! TaskListController
+        
+        // transition the task list to the controller
+        taskListController.setTasks(tasks)
+        
+        // creation of the navigation controller
+        let navigationController = UINavigationController(rootViewController: taskListController)
+        
+        // scenes' showing
+        self.window?.windowScene = windowScene
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
